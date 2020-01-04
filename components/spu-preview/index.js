118013d25e92b00a7089f8d8d48b99ca095236a0
caoke <caoke1996@qq.com>
2020-01-04 16:07:36 +0800

    新增瀑布流

--- components/spu-preview/index.js
+++ components/spu-preview/index.js
@@ -4,14 +4,34 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    data: Object
   },
 
   /**
    * 组件的初始数据
    */
   data: {
+    tags: Array
+  },
+  /**
+ * 属性监听器
+ */
+  observers: {
+    data: function (data) {
+      if (!data) {
+        return
+      }
 
+      if (!data.tags) {
+        return;
+      }
+      const tags = data.tags.split('$');//将标签字符串转换成数组
+
+      this.setData({
+        tags
+      })
+
+    }
   },
 
   /**
@@ -24,7 +44,16 @@ Component({
         w:340,
         h:340*height/width
       })
+    },
+    onItemTap(event) {
+
+      const pid = event.currentTarget.dataset.pid
+      // wx.navigateTo({
+      //   url: `/pages/detail/detail?pid=${pid}`
+      // })
+
     }
+    
 
   }
 })
